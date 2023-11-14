656435208538934f309246f60af43fbe31488a2f
isonnymichael <isonnymichael@gmail.com>
2023-11-14 18:25:59 +0700

    update: popup plugin header

--- simplepopup.php
+++ simplepopup.php
@@ -2,12 +2,12 @@
 /*
  * Plugin Name:       Simple Popup
  * Plugin URI:        https://example.com/plugins/the-basics/
- * Description:       A simple lightweight popup, fast, and powerful
+ * Description:       A simple lightweight popup, fast, and powerful. Create anything on text editor, and woolaaa.. your popup is captivating audiences effortlessly
  * Version:           0.0.1
  * Requires at least: 6.4
  * Requires PHP:      7.4
  * Author:            Sonny Michael
- * Author URI:        https://author.example.com/
+ * Author URI:        https://github.com/isonnymichael/simplepopup-framework
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Update URI:        https://example.com/my-plugin/
